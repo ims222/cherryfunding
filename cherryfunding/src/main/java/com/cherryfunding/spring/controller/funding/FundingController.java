9cf425bbdd95e2f755b5fdfdf2741b48f8e7f2e9
sh <inthisplanet@gmail.com>
2018-10-24 19:50:22 +0900

    펀딩 상세페이지에서 이미지정보 불러오기

--- cherryfunding/src/main/java/com/cherryfunding/spring/controller/funding/FundingController.java
+++ cherryfunding/src/main/java/com/cherryfunding/spring/controller/funding/FundingController.java
@@ -172,10 +172,12 @@ public class FundingController {
 
 	@RequestMapping(value = "/fundingList/detail", method = RequestMethod.GET)
 	public String detail(int num, Model model) {
-		FundingVo vo = fundingService.select(num); // 疫뀐옙 占쎄맒占쎄쉭占쎌젟癰귨옙
-		// 鈺곌퀬�돳占쎈땾 筌앹빓占쏙옙�뻻占쎄텕疫뀐옙(域뱀눖�쑓 占쎈연疫꿸퀣苑� 鈺곌퀬�돳占쎈땾揶쏉옙 占쎈툡占쎌뒄占쎈립筌욑옙 占쎌벥�눧紐꾩뵠..)
+		FundingVo vo = fundingService.select(num); // 글 하나만 조회
+		List<FPictureVo> list = fPictureService.list(num);	//펀딩번호로 이미지파일 조회
+		// 조회수 증가시켜야 하는데 조회수가 필요한지 잘 모르겠음 
 		// service.addHit(num);
 		model.addAttribute("vo", vo);
+		model.addAttribute("list",list);
 		// model.addAttribute("prev",prev);
 		// model.addAttribute("next",next);
 		return "/funding/fundingBoard/fundingDetail";
