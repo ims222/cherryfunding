8f459f041bc8484119a9a76cdd306e23d638032f
Cicadas <armorangel@naver.com>
2018-11-23 17:36:43 +0900

    봉사 이미지 서버 연결 했어요

--- cherryfunding/src/main/java/com/cherryfunding/spring/controller/volunteer/VolunteerListController.java
+++ cherryfunding/src/main/java/com/cherryfunding/spring/controller/volunteer/VolunteerListController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.cherryfunding.spring.service.volunteer.VolunteerListService;
+import com.cherryfunding.spring.util.S3Util;
 import com.cherryfunding.spring.vo.VolunteerVo;
 
 @Controller
@@ -16,11 +17,15 @@ public class VolunteerListController {
 	@Autowired
 	private VolunteerListService volunteerListService;
 	
+	@Autowired
+	private S3Util s3;
+	
 	@RequestMapping(value = "/volunteer/volunteerList", method = RequestMethod.GET)
 	public String sharingList(Model model) {
 		List<VolunteerVo> list = volunteerListService.getFirstSix();
 		for (VolunteerVo vo : list) {
-			vo.setSaveName(volunteerListService.thumbnail(vo.getvNum()).getSaveName());
+			String thumbnail = volunteerListService.thumbnail(vo.getvNum()).getSaveName();
+			vo.setSaveName(s3.getFileURL("volunteer/" + thumbnail));
 			vo.setVpInfo(volunteerListService.thumbnail(vo.getvNum()).getVpInfo());
 		}
 		model.addAttribute("list", list);
