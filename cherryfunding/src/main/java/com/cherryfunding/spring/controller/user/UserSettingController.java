8d7b2f6409c3fc23eefcb562ade185617c832692
Cicadas <armorangel@naver.com>
2018-11-25 19:53:07 +0900

    메인 이미지

--- cherryfunding/src/main/java/com/cherryfunding/spring/controller/user/UserSettingController.java
+++ cherryfunding/src/main/java/com/cherryfunding/spring/controller/user/UserSettingController.java
@@ -8,28 +8,34 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.cherryfunding.spring.service.TestLoginService;
 import com.cherryfunding.spring.service.user.UserSettingService;
 import com.cherryfunding.spring.vo.UserSettingVo;
 
 @Controller
 public class UserSettingController {
-	
+
 	@Autowired
 	private UserSettingService usService;
-	
+
+	@Autowired
+	private TestLoginService testLoginService;
+
 	@RequestMapping(value = "/userSetting", method = RequestMethod.GET)
-	public String userSetting(HttpSession session,Model model) {
-		String id=(String)session.getAttribute("id");
-		UserSettingVo vo=usService.getInfo(id);
-		model.addAttribute("vo",vo);
+	public String userSetting(HttpSession session, Model model) {
+		String id = (String) session.getAttribute("id");
+		UserSettingVo vo = usService.getInfo(id);
+		model.addAttribute("vo", vo);
 		return "user/userInfo/mypage/userSetting";
 	}
+
 	@RequestMapping(value = "/updateSettings", method = RequestMethod.POST)
-	public String updateSettings(HttpSession session,Model model,UserSettingVo vo) {
-		String id=(String)session.getAttribute("id");
+	public String updateSettings(HttpSession session, Model model, UserSettingVo vo) {
+		String id = (String) session.getAttribute("id");
 		usService.update(vo);
-		UserSettingVo vo1=usService.getInfo(id);
-		model.addAttribute("vo",vo1);
+		UserSettingVo vo1 = usService.getInfo(id);
+		model.addAttribute("vo", vo1);
+		session.setAttribute("setting", testLoginService.userSetting(id));
 		return "user/userInfo/mypage/userSetting";
 	}
 }
